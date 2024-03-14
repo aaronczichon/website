b8363d89d2ae45032831b8e82cfb4fbf9f341096
Aaron Czichon <mail@aaronczichon.de>
2024-03-14 15:27:58 +0000

    misc: formatting with prettier

--- web-canada/public/assets/scripts/map.js
+++ web-canada/public/assets/scripts/map.js
@@ -1,41 +1,42 @@
-const element = document.getElementById("map");
+const element = document.getElementById('map');
 
-  mapboxgl.accessToken = "pk.eyJ1Ijoid2ViYXRsYXMiLCJhIjoiY2xrdHB6dHg0MGVqbTNnbzR4Z3pvbTBvNCJ9.jSswMTOvnmDuLu9v9mdp9w";
-  const map = new mapboxgl.Map({
-    container: element,
-    style: "mapbox://styles/mapbox/streets-v12",
-    center: [-63.582687, 44.651070],
-    zoom: 10,
-  });
+mapboxgl.accessToken =
+	'pk.eyJ1Ijoid2ViYXRsYXMiLCJhIjoiY2xrdHB6dHg0MGVqbTNnbzR4Z3pvbTBvNCJ9.jSswMTOvnmDuLu9v9mdp9w';
+const map = new mapboxgl.Map({
+	container: element,
+	style: 'mapbox://styles/mapbox/streets-v12',
+	center: [-63.582687, 44.65107],
+	zoom: 10,
+});
 
-  map.on("load", async () => {
-    // Add zoom controls
-    map.addControl(new mapboxgl.NavigationControl());
+map.on('load', async () => {
+	// Add zoom controls
+	map.addControl(new mapboxgl.NavigationControl());
 
-    map.addLayer({
-            'id': 'circleLayer',
-            'type': 'circle',
-            'source': {
-                'type': 'geojson',
-                'data': {
-                    'type': 'Feature',
-                    'geometry': {
-                        'type': 'Point',
-                        'coordinates': [-63.582687, 44.651070]
-                    }
-                }
-            },
-            'paint': {
-                'circle-radius': {
-                    'base': 1.75,
-                    'stops': [[48, 40], [88, 720]]
-                }, // in pixels
-                'circle-color': 'blue',
-                'circle-opacity': 0.5
-            }
-        });
-    document
-      .getElementsByClassName("mapboxgl-control-container")
-      .item(0)
-      .remove();
-  });
+	map.addLayer({
+		id: 'circleLayer',
+		type: 'circle',
+		source: {
+			type: 'geojson',
+			data: {
+				type: 'Feature',
+				geometry: {
+					type: 'Point',
+					coordinates: [-63.582687, 44.65107],
+				},
+			},
+		},
+		paint: {
+			'circle-radius': {
+				base: 1.75,
+				stops: [
+					[48, 40],
+					[88, 720],
+				],
+			}, // in pixels
+			'circle-color': 'blue',
+			'circle-opacity': 0.5,
+		},
+	});
+	document.getElementsByClassName('mapboxgl-control-container').item(0).remove();
+});
--- web-canada/src/pages/rss.xml.js
+++ web-canada/src/pages/rss.xml.js
@@ -2,20 +2,20 @@ import { getCollection } from 'astro:content';
 import rss from '@astrojs/rss';
 
 export async function GET(_context) {
-  const blog = await getCollection('blog');
-  return rss({
-    title: 'Canada - Working Holiday für 12 Monate - Blog',
-    // `<description>` field in output xml
-    description: 'Für 12 Monate reisend und arbeitend durch Kanada.',
-    site: 'https://canada.aaronczichon.de',
-    items: blog.map((post) => ({
-      title: post.data.title,
-      pubDate: post.data.pubDate,
-      description: post.data.description,
-      customData: post.data.customData,
-      // Compute RSS link from post `slug`
-      // This example assumes all posts are rendered as `/blog/[slug]` routes
-      link: `/blog/${post.slug}/`,
-    })),
-  });
+	const blog = await getCollection('blog');
+	return rss({
+		title: 'Canada - Working Holiday für 12 Monate - Blog',
+		// `<description>` field in output xml
+		description: 'Für 12 Monate reisend und arbeitend durch Kanada.',
+		site: 'https://canada.aaronczichon.de',
+		items: blog.map((post) => ({
+			title: post.data.title,
+			pubDate: post.data.pubDate,
+			description: post.data.description,
+			customData: post.data.customData,
+			// Compute RSS link from post `slug`
+			// This example assumes all posts are rendered as `/blog/[slug]` routes
+			link: `/blog/${post.slug}/`,
+		})),
+	});
 }
