b28ec6b1c2726b88228b2c049eed521285551755
Aaron Czichon <mail@aaronczichon.de>
2024-03-21 21:00:50 +0000

    feat: include full markdown

--- personal/src/pages/rss.xml.js
+++ personal/src/pages/rss.xml.js
@@ -1,18 +1,22 @@
 import { getCollection } from 'astro:content';
 import rss from '@astrojs/rss';
+import sanitizeHtml from 'sanitize-html';
+import MarkdownIt from 'markdown-it';
+const parser = new MarkdownIt();
 
 export async function GET(_context) {
 	const blog = await getCollection('blog');
 	return rss({
-		title: 'Canada - Working Holiday für 12 Monate - Blog',
+		title: 'Aaron Czichon - Blog - RSS',
 		// `<description>` field in output xml
-		description: 'Für 12 Monate reisend und arbeitend durch Kanada.',
-		site: 'https://canada.aaronczichon.de',
+		description: 'Aaron\' Blog as RSS feed',
+		site: 'https://aaronczichon.de',
 		items: blog.map((post) => ({
 			title: post.data.title,
 			pubDate: post.data.pubDate,
 			description: post.data.description,
 			customData: post.data.customData,
+			content: sanitizeHtml(parser.render(post.body)),
 			// Compute RSS link from post `slug`
 			// This example assumes all posts are rendered as `/blog/[slug]` routes
 			link: `/blog/${post.slug}/`,
