d76d56aea7bc999cb13b2fa4b48b5b09600c326a
Aaron Czichon <mail@aaronczichon.de>
2024-11-15 01:02:58 +0000

    feat: returning form data as submitted
    refs: #36

--- comment-submit/src/index.ts
+++ comment-submit/src/index.ts
@@ -1,14 +1,17 @@
 import { defineEndpoint } from '@directus/extensions-sdk';
 import { Request, Response } from 'express';
+import multer from 'multer';
+
+const upload = multer();
 
 const handler = async (req: Request, res: Response) => {
 	const host = req.headers.host;
 	if (!host?.includes('aaronczichon.de')) {
 		return res.status(403).send('Forbidden');
 	}
-	return res.status(200).send(req.body);
+	return res.status(200).json(req.body).send();
 };
 
 export default defineEndpoint((router) => {
-	router.post('/', (_req, res) => handler(_req, res));
+	router.post('/', upload.none(), (_req, res) => handler(_req, res));
 });
