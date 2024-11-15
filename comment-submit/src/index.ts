6ec330423db96bd69b7f10a0d22024ffbb0b8239
Aaron Czichon <mail@aaronczichon.de>
2024-11-15 00:44:03 +0000

    feat: adding directus extension for comment submit
    refs: #36

---
+++ comment-submit/src/index.ts
@@ -0,0 +1,14 @@
+import { defineEndpoint } from '@directus/extensions-sdk';
+import { Request, Response } from 'express';
+
+const handler = async (req: Request, res: Response) => {
+	const host = req.headers.host;
+	if (!host?.includes('aaronczichon.de')) {
+		return res.status(403).send('Forbidden');
+	}
+	return res.status(200).send('Hello World');
+};
+
+export default defineEndpoint((router) => {
+	router.get('/', (_req, res) => handler(_req, res));
+});
